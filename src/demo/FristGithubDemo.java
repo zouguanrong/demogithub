e980d5c267b497205e2f928c6478e39e2202ed92
zouguanrong <792482426@qq.com>
2020-06-30 20:17:55 +0800

    first commit

---
+++ src/demo/FristGithubDemo.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * 第一次上传git项目
+ */
+public class FristGithubDemo {
+
+    public static void main(String[] args) {
+        System.out.println("FristGithubDemo...");
+    }
+
+}
